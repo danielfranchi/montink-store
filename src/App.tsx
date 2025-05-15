5e3576876a43e82d8cd86b96fad532cf82558d84
daniel <daniel2mf@yahoo.com.br>
2025-05-15 01:07:59 -0300

    montink-store

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header";
 import ProductView from "./components/ProductView";
 
 const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <ProductView />
+    <div className="min-h-screen flex flex-col bg-gray-100">
+      <Header /> {/* ✅ Agora o Header está no topo */}
+      <div className="flex flex-grow items-center justify-center">
+        <ProductView />
+      </div>
     </div>
   );
 };
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-black text-gray-300 py-3 px-6 flex justify-center items-center shadow-md">
+      <h1 className="text-lg font-bold tracking-wide font-roboto">
+        Montink-Store
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
